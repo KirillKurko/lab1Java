1d3323eb47efca9b83cca29ea94c8bcd524bca04
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-05 09:08:20 +0300

    printCitiesTotalArea WriterInfo method implemented

--- src/classes/WriterInfo.java
+++ src/classes/WriterInfo.java
@@ -2,21 +2,25 @@ package classes;
 
 public class WriterInfo {
 
-    public void printCityName(City city) {
+    public static void printCityName(City city) {
         System.out.println("Name: " + city.getName());
     }
 
-    public void printCityPopulation(City city) {
+    public static void printCityPopulation(City city) {
         System.out.println("Population: " + city.getPopulation());
     }
 
-    public void printCityArea(City city) {
+    public static void printCityArea(City city) {
         System.out.println("Area: " + city.getArea());
     }
 
-    public void printCityInformation(City city) {
+    public static void printCityInformation(City city) {
        printCityName(city);
        printCityPopulation(city);
        printCityArea(city);
+    }
+
+    public static void printSitiesTotalArea(City[] cities) {
+        System.out.println("Cities total area: " +  CityInformationHelper.calculateTotalArea(cities));
     }
 }
